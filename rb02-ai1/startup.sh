9799ee16aaab4f190c23748dba4415b50e24c9cd
marcostrullato <marco.strullato@gmail.com>
2020-11-13 21:30:10 +0100

    fixed rb02-ai1 startup script

--- rb02-ai1/startup.sh
+++ rb02-ai1/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 screen -d -m roscore
 screen -d -m /bin/bash -c '. /create_ws/devel/setup.bash; roslaunch create_bringup create_2.launch'
+
+docker run --device=/dev/ttyUSB0 --device /dev/bus/usb:/dev/bus/usb -i -t --entrypoint /bin/bash images.strullato.com:8080/rb02-all-in-one:1.1
