ba0fa4c50a72e5119a59fb5546bca4fe46298595
marcostrullato <marco.strullato@gmail.com>
2020-11-15 21:26:25 +0100

    fixed rb02-ai1 Docker file

--- rb02-ai1/startup.sh
+++ rb02-ai1/startup.sh
@@ -5,4 +5,4 @@ screen -d -m /bin/bash -c '. /kinect_ws/devel/setup.bash; roslaunch freenect_lau
 screen -d -m /bin/bash -c '. /vision_ws/devel/setup.bash; roslaunch darknet_ros darknet_ros.launch'
 screen -d -m /bin/bash -c '. /create_ws/devel/setup.bash; roslaunch create_bringup joy_teleop.launch'
 
-docker run --device=/dev/ttyUSB0 --device /dev/bus/usb:/dev/bus/usb -i -t --entrypoint /bin/bash images.strullato.com:8080/rb02-all-in-one:1.1
+docker run --device=/dev/ttyUSB0 --device /dev/bus/usb:/dev/bus/usb -i -t --entrypoint /bin/bash --network host images.strullato.com:8080/rb02-all-in-one:1.2
