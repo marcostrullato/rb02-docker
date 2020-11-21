42622c82c1c72420d12356e2ae30f65c5ee2be83
marcostrullato <marco.strullato@gmail.com>
2020-11-21 21:23:48 +0100

    fixed rb02-control Docker file and added startup file

---
+++ rb02-control/startup.sh
@@ -0,0 +1,6 @@
+docker run --device=/dev/input/js0 -i -t --entrypoint /bin/bash --network host images.strullato.com:8080/rb02-control:1.1
+
+export ROS_MASTER_URI=http://192.168.1.15:11311
+export ROS_IP=192.168.1.115
+roslaunch create_bringup joy_teleop.launch joy_config:=gamepad
+
