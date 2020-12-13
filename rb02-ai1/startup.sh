86660971f3f16412ba48bc240b03a3f37c7ad8fe
marcostrullato <marco.strullato@gmail.com>
2020-12-13 20:17:31 +0100

    update few files

--- rb02-ai1/startup.sh
+++ rb02-ai1/startup.sh
@@ -5,6 +5,7 @@ export ROS_MASTER_URI=http://192.168.1.15:11311
 
 screen -d -m roscore
 screen -d -m /bin/bash -c '. /create_ws/devel/setup.bash; roslaunch create_bringup create_2.launch'
+#screen -d -m /bin/bash -c '. /kinect_ws/devel/setup.bash; roslaunch freenect_launch freenect_throttle.launch rate:=5'
 screen -d -m /bin/bash -c '. /kinect_ws/devel/setup.bash; roslaunch freenect_launch freenect.launch depth_registration:=true'
 screen -d -m /bin/bash -c '. /kinect_ws/devel/setup.bash; roslaunch rtabmap_ros rgbd_mapping.launch rtabmap_args:="--delete_db_on_start --Vis/MaxFeatures 500 --Mem/ImagePreDecimation 2 --Mem/ImagePostDecimation 2 --Kp/DetectorStrategy 6 --OdomF2M/MaxSize 1000 --Odom/ImageDecimation 2" rtabmapviz:=false'
 screen -d -m /bin/bash -c '. /vision_ws/devel/setup.bash; roslaunch darknet_ros darknet_ros.launch'
@@ -13,4 +14,4 @@ screen -d -m /bin/bash -c '. /vision_ws/devel/setup.bash; roslaunch darknet_ros 
 
 screen -d -m /bin/bash -c '. /opt/ros/melodic/setup.bash; . /create_ws/devel/setup.bash --extend; . /kinect_ws/devel/setup.bash --extend; . /vision_ws/devel/setup.bash --extend; . /rb02_ws/devel/setup.bash --extend; roslaunch roomba roomba.launch'
 
-docker run --device=/dev/ttyUSB0 --device /dev/bus/usb:/dev/bus/usb --runtime nvidia -i -t --entrypoint /bin/bash --network host images.strullato.com:8080/rb02-all-in-one:1.2
+docker run --device=/dev/ttyUSB0 --device /dev/bus/usb:/dev/bus/usb --runtime nvidia -i -t --entrypoint /bin/bash --network host images.strullato.com:8080/rb02-all-in-one:1.4
--- rb02-control/startup.sh
+++ rb02-control/startup.sh
@@ -3,3 +3,5 @@ export ROS_IP=192.168.1.115
 
 screen -d -m /bin/bash -c '. /create_ws/devel/setup.bash; ROS_MASTER_URI=http://192.168.1.15:11311; ROS_IP=192.168.1.115; roslaunch create_bringup joy_teleop.launch joy_config:=gamepad'
 
+
+rosrun rtabmap_ros rtabmapviz
