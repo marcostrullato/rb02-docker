a5c41e15c09e102f20529f62447f6b110b4e3a9c
marcostrullato <marco.strullato@gmail.com>
2020-12-13 21:07:48 +0100

    updated rb02-control/startup.sh file

--- rb02-control/startup.sh
+++ rb02-control/startup.sh
@@ -3,5 +3,5 @@ export ROS_IP=192.168.1.115
 
 screen -d -m /bin/bash -c '. /create_ws/devel/setup.bash; ROS_MASTER_URI=http://192.168.1.15:11311; ROS_IP=192.168.1.115; roslaunch create_bringup joy_teleop.launch joy_config:=gamepad'
 
+roslaunch rtabmap_ros rtabmap.launch subscribe_rgbd:=true rgbd_topic:=/camera/rgbd_image compressed:=true rtabmap_args:="--delete_db_on_start"
 
-rosrun rtabmap_ros rtabmapviz
